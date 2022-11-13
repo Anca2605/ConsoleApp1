f3bf6b60772f94a3e185c3c1a8484009a499404c
Ancaelena2605 <112935366+Ancaelena2605@users.noreply.github.com>
2022-11-13 14:51:48 +0100

    refactored pirnt outs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Jetta e blana");
-Console.WriteLine("Jetta e blana si merge tare");
